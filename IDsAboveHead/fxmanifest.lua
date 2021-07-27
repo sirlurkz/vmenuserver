fdfb24e9d8f024d7ca1b052b5c9c3431d30be6c7
sirlurkz <82527024+sirlurkz@users.noreply.github.com>
2021-07-27 18:36:10 +0100

    Add files via upload

---
+++ IDsAboveHead/fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'rubbertoe98'
+description 'IDsAboveHead'
+version '1.0.0'
+
+client_script "cl_idsabovehead.lua"
+
+client_script "@RDRP-AntiCheat/acloader.lua"
